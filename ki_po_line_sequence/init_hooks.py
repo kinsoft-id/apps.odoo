7d4eedb1eb355a5a6cfd2bd495181510f4d18470
kikin <kikin.kusumah@gmail.com>
2019-11-11 08:53:28 +0700

    fix hook

--- ki_po_line_sequence/init_hooks.py
+++ ki_po_line_sequence/init_hooks.py
@@ -8,8 +8,8 @@ from odoo.api import Environment
 
 def post_init_hook(cr, pool):
     """
-    Fetches all the sale order and resets the sequence of the order lines
+    Fetches all the purchase order and resets the sequence of the order lines
     """
     env = Environment(cr, SUPERUSER_ID, {})
-    sale = env['sale.order'].search([])
-    sale._reset_sequence()
+    purchase = env['purchase.order'].search([])
+    purchase._reset_sequence()
