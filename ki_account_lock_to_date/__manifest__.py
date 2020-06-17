a8886a472f850a56c7ff3bc96dcb51b8fcc9c55f
Kikin Kusumah <kikin.kusumah@gmail.com>
2020-06-17 13:38:31 +0700

    add account lock date module

---
+++ ki_account_lock_to_date/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2019 ForgeFlow S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Account Lock To Date',
+    'summary': """
+        Allows to set an account lock date in the future.""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': "Kikin Kusumah@Kinsoft Indonesia, " 
+			  "ForgeFlow, Odoo Community Association (OCA), "
+    'website': 'kinsoft.id, https://github.com/OCA/account-financial-tools',
+    'installable': True,
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'wizards/account_update_lock_to_date.xml',
+    ],
+}
